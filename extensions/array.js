897753dd0d9bafd6562b060321af9fc2c40d3643
Blake Randall <blake.randall.oakville@gmail.com>
2016-12-25 20:54:10 -0500

    fixed

--- extensions/array.js
+++ extensions/array.js
@@ -1,5 +1,15 @@
+function arrayFlatten(array) {
+    return array.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? arrayFlatten(toFlatten) : toFlatten);
+    }, []);
+}
+
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)];
+}
+
+Array.prototype.flatten = function () {
+    return arrayFlatten(this);
 }
 
 Object.defineProperty(Array.prototype, "randomElement", {
@@ -8,4 +18,13 @@ Object.defineProperty(Array.prototype, "randomElement", {
     }
 });
 
-module.exports = (array) => array[Math.floor(Math.random() * array.length)];
+Object.defineProperty(Array.prototype, "flatten", {
+    get: function flatten() {
+        return arrayFlatten(this);
+    }
+});
+
+module.exports = {
+    random: (array) => array[Math.floor(Math.random() * array.length)],
+    flatten: (array) => arrayFlatten(array)
+};
--- index.js
+++ index.js
@@ -7,61 +7,59 @@ const config = require(path.join(__dirname, 'config.json'));
 const db = require(path.join(__dirname, 'models'));
 const erisapi = require("eris");
 const client = new erisapi.CommandClient(config.Token, {}, {
-	defaultHelpCommand: true,
-	description: 'Advanced Automated Reaction Bot',
-	ignoreBots: true,
-	ignoreSelf: true,
-	name: config.Nickname,
-	owner: config.Owner,
-	prefix: config.CommandPrefix,
-	defaultCommandOptions: {
-		caseInsensitive: true,
-		deleteCommand: true,
-		argsRequired: false,
-		guildOnly: false,
-		dmOnly: false,
-		description: 'No description',
-		fullDescription: 'No full description',
-		usage: '',
-		requirements: {
-			userIDs: [
-				config.OwnerId
-			],
-			permissions: {
-				"administrator": true
-			},
-			roleIDs: [
-			],
-			roleNames: [
-				'reactions'
-			]
-		},
-		cooldown: 2000,
-		cooldownMessage: 'Cooldown hit!',
-		permissionMessage: 'Insufficient Permissions'
-	}
+    defaultHelpCommand: true,
+    description: 'Advanced Automated Reaction Bot',
+    ignoreBots: true,
+    ignoreSelf: true,
+    name: config.Nickname,
+    owner: config.Owner,
+    prefix: config.CommandPrefix,
+    defaultCommandOptions: {
+        caseInsensitive: true,
+        deleteCommand: true,
+        argsRequired: false,
+        guildOnly: false,
+        dmOnly: false,
+        description: 'No description',
+        fullDescription: 'No full description',
+        usage: '',
+        requirements: {
+            userIDs: [
+                config.OwnerId
+            ],
+            permissions: {
+                "administrator": true
+            },
+            roleIDs: [
+            ],
+            roleNames: [
+                'reactions'
+            ]
+        },
+        cooldown: 2000,
+        cooldownMessage: 'Cooldown hit!',
+        permissionMessage: 'Insufficient Permissions'
+    }
 });
+
 const commands = require(path.join(__dirname, 'commands'))(client);
 
 function _ReactionToMessage(msg) {
-	db.models.Keyword.findAll({
-		include: [{
-			model: db.models.Emoji
-		}]
-	}).then((keywords) => {
-		var emojis = keywords.filter((keyword) => extensions.regex(keyword.get('Regex')).test(msg.content))
-			.map((keyword) => keyword.get('Emojis')
-				.map((emoji) => emoji.get('Code')).join(''));
-		emojis.forEach((e) => {
-			client.addMessageReaction(msg.channel.id, msg.id, e).then((msg) => { }, (err) => { console.log(err) });
-		});
-	}, (err) => { });
+    db.models.Keyword.findAll({
+        include: [{
+            model: db.models.Emoji
+        }]
+    }).then((keywords) => {
+        var emojis = keywords.filter((keyword) => extensions.regex(keyword.get('Regex')).test(msg.content))
+            .map((keyword) => keyword.get('Emojis'))
+            .flatten
+            .map((emoji) => ({ name: emoji.get('Name'), code: emoji.get('Code') }));
+        emojis.forEach((e) => client.addMessageReaction(msg.channel.id, msg.id, e.code).then((msg) => { }, (err) => { }));
+    }, (err) => { });
 }
 
 client.on('ready', () => {
-	client.guilds.forEach((guild) => {
-		client.editNickname(guild.id, config.Nickname).then((success) => { }, (err) => { });
-	});
+    client.guilds.forEach((guild) => client.editNickname(guild.id, config.Nickname).then((success) => { }, (err) => { }));
 });
 
 client.on('connect', () => {
@@ -74,43 +72,43 @@ client.on('disconnect', () => {
 });
 
 client.on('guildMemberUpdate', (guild, member, oldMember) => {
-	if (member.user == client.user) {
-		client.editNickname(guild.id, config.Nickname).then((success) => { }, (err) => { });
-	}
+    if (member.user == client.user) {
+        client.editNickname(guild.id, config.Nickname).then((success) => { }, (err) => { });
+    }
 });
 
 client.on('messageCreate', (msg) => {
-	if (msg.author != client.user) {
-		_ReactionToMessage(msg);
-	}
+    if (msg.author != client.user) {
+        _ReactionToMessage(msg);
+    }
 });
 
 client.on('messageUpdate', (newMsg, oldMsg) => {
-	if (oldMsg.author != client.user && newMsg.author != client.user) {
-		_ReactionToMessage(newMsg);
-	}
+    if (oldMsg.author != client.user && newMsg.author != client.user) {
+        _ReactionToMessage(newMsg);
+    }
 });
 
 client.on('messageDelete', (msg) => {
-	if (msg.author != client.user) {
+    if (msg.author != client.user) {
 
-	}
+    }
 });
 
 client.on('messageReactionAdd', (msg, emoji, userid) => {
-	if (msg.author != client.user) {
-		client.addMessageReaction(msg.channel.id, msg.id, emoji.name).then((msg) => { }, (err) => { });
-	}
+    if (msg.author != client.user) {
+        client.addMessageReaction(msg.channel.id, msg.id, emoji.name).then((msg) => { }, (err) => { });
+    }
 });
 
 client.on('messageReactionRemove', (msg, emoji, userid) => {
-	if (msg.author != client.user) {
-		client.removeMessageReaction(msg.channel.id, msg.id, emoji.name).then((msg) => { }, (err) => { });
-	}
+    if (msg.author != client.user) {
+        client.removeMessageReaction(msg.channel.id, msg.id, emoji.name).then((msg) => { }, (err) => { });
+    }
 });
 
 process.on('SIGINT', function () {
-	client.disconnect({ reconnect: false });
+    client.disconnect({ reconnect: false });
 });
 
 client.connect();
